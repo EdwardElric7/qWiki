e4e9081a803d78da9738309a5a781a10f8fddc11
jbodi <juanluisbodi@gmail.com>
2017-05-31 17:45:00 +0200

    Intento de autobúsqueda para que Iulian continúe.

--- src/app/searchBar.component.ts
+++ src/app/searchBar.component.ts
@@ -1,4 +1,6 @@
-import {Component, Output, EventEmitter, ViewChild, ElementRef, OnInit} from '@angular/core';
+import {Component, Output, EventEmitter, ViewChild, ElementRef, OnInit, Injectable} from '@angular/core';
+import 'rxjs/add/operator/toPromise';
+import {Headers, Http} from "@angular/http";
 
 @Component({
   selector: 'app-search-bar',
@@ -11,6 +13,12 @@ export class SearchBarComponent implements OnInit {
 
   @ViewChild('searchBox') searchBox: ElementRef;
 
+  auto: Auto = new Auto();
+  searchTerm: string;
+
+
+  constructor(private autoService: AutoService){}
+
   ngOnInit() {
     this.searchBox.nativeElement.focus();
   }
@@ -20,6 +28,36 @@ export class SearchBarComponent implements OnInit {
       searchTerm = searchTerm.split('/')[4];
     }
 
-    this.search.next(searchTerm);
+    this.search.next(searchTerm)
+  }
+
+  searchAuto(searchTerm) {
+    this.searchTerm = searchTerm;
+    this.autoService.getAuto('auto/' + this.searchTerm).then(auto => {
+      this.auto = auto;
+      console.log(JSON.stringify(this.auto));
+    });
+  }
+}
+
+class Auto{}
+
+@Injectable()
+class AutoService{
+  private headers = new Headers({'Content-Type': 'application/json'});
+  private articleUrl = 'http://localhost:8080';
+
+
+  constructor(private http: Http) {
+  }
+  getAuto(search: String): Promise<Auto> {
+    const url = `${this.articleUrl}/${search}`;
+    return this.http.get(url).toPromise().then(response => response.json() as Auto)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 }
